5164fe0a3e3f8b563af0e5070411fb5d022598fc
uncertainty2019 <maoyijia200616@126.com>
2020-03-03 16:24:32 +0800

    dice

--- Violet_Yisi/Test.py
+++ Violet_Yisi/Test.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+import random
+message = input(">")
+judgement = message.split(" ")
+if judgement[0] == "check":
+    try:
+        capacity = int(judgement[1])
+        value = random.randint(1, 100)
+        subject = judgement[2]
+        if value <= capacity:
+            if value <= 5:
+                print(f"{subject} {value}/{capacity} Wow! A great success!")
+            else:
+                print(f"{subject} {value}/{capacity} Congratulations!")
+        else:
+            if value > 95:
+                print(f"{subject} {value}/{capacity} That's so bad! A great failure!")
+            else:
+                print(f"{subject} {value}/{capacity} I feel sorry to tell you that you fail.")
+    except ValueError:
+        print("Invalid entering!")
+else:
+    print("Invalid entering!")
