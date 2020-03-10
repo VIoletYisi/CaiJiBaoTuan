1b59e2aa831f27bfec7c76fe8a6bb8ca5a4c1bbf
VioletYisi <657114184@qq.com>
2020-03-10 10:50:07 +0800

    Nothing

--- dice.py
+++ dice.py
@@ -1,4 +1,4 @@
-
+#%%
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
@@ -6,3 +6,4 @@ import numpy as np
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
 plt.show() 
+
