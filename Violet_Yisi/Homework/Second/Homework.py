484743f04db05dd9b491028648116f7d8ac9bc81
VioletYisi <657114184@qq.com>
2020-03-13 20:32:35 +0800

    Homwork

--- Violet_Yisi/Homework/Second/Homework.py
+++ Violet_Yisi/Homework/Second/Homework.py
@@ -59,6 +59,7 @@ p = n * np.sqrt(0.25 + 0.25 - 2 * 0.5 * 0.5 * np.cos(2 * np.pi / n))
 plt.plot(n,p)
 
 # %%
+np.set_printoptions(precision=8)
 y = np.empty((1,21))
 t = np.empty((1,21))
 y[0,0] = 10
@@ -73,4 +74,31 @@ for i in range(0,30):
 print(y)
 print(t)
 list(zip(t,y))
+# %%
+v_average = np.zeros((1,20))
+for i in range(20):
+    v_average[0, i] = (y[0,i] - y[0,i + 1]) / (t[0,i + 1] - t[0,i])
+print(v_average)
+
+# %%
+t_mid = np.zeros((1,20))
+a_average = np.zeros((1,19)) 
+for i in range(20):
+    t_mid[0,i] = (t[0,i] + t[0,i + 1]) / 2
+
+for i in range(19):
+    a_average[0,i] = (v_average[0,i + 1] - v_average[0,i]) / (t_mid[0,i + 1] - t_mid[0,i]) 
+print(a_average)
+
+# %%
+t = np.arange(0, 1.5, 0.2)
+v = 9.8 * t
+plt.plot(t,v)
+
+
+# %%
+t = np.arange(0, 1.5, 0.2)
+a = 9.8+0 * t
+plt.plot(t,a)
+
 # %%
