9e84edfb15511d44477e031e2dddb9088e659c29
VioletYisi <657114184@qq.com>
2020-03-22 22:29:06 +0800

    Homework By Violet_Yisi

--- Violet_Yisi/Homework/Third/Homework.py
+++ Violet_Yisi/Homework/Third/Homework.py
@@ -1,1 +1,25 @@
 #%%
+s = 0
+e = 14 
+f = 1.625
+p = e - 127
+A = (-1) ** s * f * 2 ** p
+print(A)
+
+# %%
+import sys
+max = float('inf')
+over = 1.
+while over != max:
+    over = over * 2.
+print(over) 
+# %%
+eps = 1.
+one = 1
+while one + eps != 1:
+    eps = eps / 2.
+    one = 1
+print(eps)
+
+
+# %%
